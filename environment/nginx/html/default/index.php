c3558c2d310de844184e301bff59a15821a6d755
Mrdai <346139312@qq.com>
2017-11-07 17:00:27 +0800

    环境调试

---
+++ environment/nginx/html/default/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/7
+ * Time: 8:50
+ */
+//$servername = "localhost";
+//$username = "root";
+//$password = "root";
+//
+//try {
+//    $conn = new PDO("mysql:host=$servername;dbname=hosts", $username, $password);
+//    echo "连接成功";
+//}
+//catch(PDOException $e)
+//{
+//    echo $e->getMessage();
+//}
+phpinfo();
+//$dbconn2 = pg_connect("host=localhost port=5432 dbname=cloud");
--- environment/php-apache/html/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mrdai
- * Date: 17-11-6
- * Time: 下午9:25
- */
-phpinfo();
