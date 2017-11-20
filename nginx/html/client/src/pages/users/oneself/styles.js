f485d58b29909357893f017bded88c427fea21b3
Mrdaigithub <346139312@qq.com>
2017-11-20 16:42:45 +0800

    拆分组件

--- nginx/html/client/src/pages/users/oneself/styles.js
+++ nginx/html/client/src/pages/users/oneself/styles.js
@@ -25,26 +25,6 @@ const styles = theme => ({
         color: '#555',
         fontWeight: 500,
         textAlign: 'center',
-    },
-    'global-footer': {
-        width: '100%',
-        textAlign: 'center',
-        position: 'absolute',
-        bottom: '0',
-        left: '0'
-    },
-    'links': {
-        display: 'inline-block',
-        marginRight: '10px',
-        fontSize: '14px',
-        color: '#555',
-        fontWeight: 100,
-        textDecoration:'none'
-    },
-    'copyright': {
-        fontSize: '14px',
-        color: '#555',
-        fontWeight: 100
     }
 });
 
--- nginx/html/client/src/routes/index.js
+++ nginx/html/client/src/routes/index.js
@@ -4,6 +4,7 @@ import {
     Route,
 } from 'react-router-dom'
 import PageHeaderLayout from '../layouts/PageHeaderLayout/PageHeaderLayout'
+import BasicLayout from '../layouts/BasicLayout/BasicLayout'
 import Login from '../pages/login/Login'
 import CloudDrive from '../pages/cloud-drive/CloudDrive'
 import Oneself from '../pages/users/oneself/Oneself'
@@ -11,9 +12,9 @@ import Oneself from '../pages/users/oneself/Oneself'
 const Routes = () => (
     <Router>
         <div>
-            <Route path="/login" component={Login}/>
+            <Route path="/login" render={() => <BasicLayout><Login/></BasicLayout>}/>
             <Route path="/cloud-drive" render={() => <PageHeaderLayout><CloudDrive/></PageHeaderLayout>}/>
-            <Route path="/users/oneself" component={Oneself}/>
+            <Route path="/users/oneself" render={() => <BasicLayout><Oneself/></BasicLayout>}/>
         </div>
     </Router>
 )
