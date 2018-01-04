3f72ec0e47894e4f73d6a2d972a957d0bf2d1fba
Mrdaigithub <346139312@qq.com>
2018-01-04 12:31:44 +0800

    前端添加路由保护

---
+++ client/src/routes/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import CloudDrive from '../pages/CloudDrive';
+import Oneself from '../pages/Personnel/Oneself';
+import Groups from '../pages/Personnel/Groups';
+import NotFound from '../pages/NotFound';
+
+const PrivateRoute = () => (
+    <div>
+        {
+            (sessionStorage.accessToken && sessionStorage.refreshToken) ?
+                (<Switch>
+                    <Route path="/cloud-drive/0" component={CloudDrive}/>
+                    <Redirect form="/cloud-drive" to="/cloud-drive/0"/>
+                    <Route exact path="/personnel/oneself" component={Oneself}/>
+                    <Route exact path="/personnel/groups" component={Groups}/>
+                    <Route component={NotFound}/>
+                </Switch>) :
+                (<Redirect to="/login"/>)
+        }
+    </div>
+);
+
+export default PrivateRoute;
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import Welcome from '../pages/Welcome';
 import Login from '../pages/Login';
-import CloudDrive from '../pages/CloudDrive';
-import Oneself from '../pages/Personnel/Oneself';
-import Groups from '../pages/Personnel/Groups';
 import NotFound from '../pages/NotFound';
 
 const Routes = props => (
@@ -12,11 +10,7 @@ const Routes = props => (
         <Route exact path="/" component={Welcome}/>
         <Route exact path="/welcome" component={Welcome}/>
         <Route exact path="/login" component={Login}/>
-        <Route path="/cloud-drive/0" component={CloudDrive}/>
-        <Redirect exact from="/cloud-drive" to="/cloud-drive/0"/>
-        <Route exact path="/personnel/oneself" component={Oneself}/>
-        <Route exact path="/personnel/groups" component={Groups}/>
-        <Route component={NotFound}/>
+        <PrivateRoute/>
     </Switch>
 );
 
