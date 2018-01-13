0c7c773f0a23409856aad41af4f8f8acd193b56a
Mrdaigithub <346139312@qq.com>
2018-01-13 12:37:29 +0800

    update

---
+++ client/src/pages/Trash/styles.js
@@ -0,0 +1,28 @@
+const styles = theme => ({
+        normal: {
+            height: '100vh',
+            padding: '60px 0 5px 0',
+            boxSizing: 'border-box',
+        },
+        bottomBarBtnIcon: {
+            color: '#fff',
+        },
+        SpeedDialItemInput: {
+            display: 'none',
+        },
+        SpeedDialItemButton: {
+            display: 'inline',
+        },
+        resourceIcon: {
+            marginRight: 0,
+        },
+        moveDirTopBar: {
+            marginBottom: '20px',
+        },
+        moveDirTopBarTitle: {
+            flex: 1,
+        },
+    })
+;
+
+export default styles;
