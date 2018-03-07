b9bd73fcd3b82e56b8eaf1bca0875769a078802f
Mrdai <346139312@qq.com>
2018-03-07 17:00:47 +0800

    update

--- fss/src/main.go
+++ fss/src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+//func SayHello(w http.ResponseWriter, req *http.Request) {
+//	w.Write([]byte("Hello"))
+//	fmt.Print("hello")
+//}
 
 func main() {
 	fmt.Print("hello")
+
+	//http.HandleFunc("/hello", SayHello)
+	//http.ListenAndServe(":8001", nil)
 }
