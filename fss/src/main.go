14b78df6790f9576142a1a104a605e90d8eb578e
Mrdai <346139312@qq.com>
2018-03-08 10:51:12 +0800

    添加golang container

--- fss/src/main.go
+++ fss/src/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-//func SayHello(w http.ResponseWriter, req *http.Request) {
-//	w.Write([]byte("Hello"))
-//	fmt.Print("hello")
-//}
+func SayHello(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello"))
+	fmt.Print("hello\n")
+}
+
 
 func main() {
-	fmt.Print("hello")
-
-	//http.HandleFunc("/hello", SayHello)
-	//http.ListenAndServe(":8001", nil)
+	http.HandleFunc("/hello", SayHello)
+	http.ListenAndServe(":8010", nil)
 }
